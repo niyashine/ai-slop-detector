b64a3551727142970ac69a4b5f5f1bb2138b1697
niyashine <niyashine.9e@gmail.com>
2026-06-19 14:54:03 +0530

    Backend stable: Flask API working with dummy model

---
+++ model/model_loader.py
@@ -0,0 +1,9 @@
+def predict(text):
+    return {
+        "prediction": "Test Prediction",
+        "confidence": 75,
+        "reasons": [
+            "Backend is working",
+            "Model is placeholder"
+        ]
+    }
